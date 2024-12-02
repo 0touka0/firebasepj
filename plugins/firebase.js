e1ddf5e026ab07ab686991683753f40b93d67f87
touka <touka6463@gmail.com>
2024-12-02 17:59:10 +0900

    .env追加

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 if (!firebase.apps.length) {
     firebase.initializeApp({
-        apiKey: "AIzaSyD3hR9OvMEezKXDOm17p7LPq6TZAqQ6_c0",
-        authDomain: "fir-pj-2987c.firebaseapp.com",
-        projectId: "fir-pj-2987c",
-        storageBucket: "fir-pj-2987c.firebasestorage.app",
-        messagingSenderId: "30486258127",
-        appId: "1:30486258127:web:8616796cda8776e724b2ba",
-        measurementId: "G-8GWF7GLRPZ",
+        apiKey: process.env.VUE_APP_API_KEY,
+        authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+        projectId: process.env.VUE_APP_PROJECT_ID,
+        storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+        messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+        appId: process.env.VUE_APP_APP_ID,
+        measurementId: process.env.VUE_APP_MEASUREMENT_ID,
     });
 }
 
